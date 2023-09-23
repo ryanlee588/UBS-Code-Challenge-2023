00dadb4f3fb0a4811fbc7abb3e5b073222797ae1
ryanlsh <ryan@u.yale-nus.edu.sg>
2023-09-23 11:36:38 +0800

    feat: greedy monkey 3d array instead of 2d

--- routes/greedy_monkey.py
+++ routes/greedy_monkey.py
@@ -16,14 +16,16 @@ def greedy_monkey():
     v = data["v"]
     f = data["f"]
     n = len(f)
-    dp = [[0] * (v + 1) for _ in range(w + 1)]
+    dp = [[[0] * (v + 1) for _ in range(w + 1)] for _ in range(n + 1)]
 
-    for i in range(n):
-        weight, volume, value = f[i]
-        for j in range(w, 0, -1):
-            for k in range(v, 0, -1):
+    for i in range(1, n + 1):
+        weight, volume, value = f[i - 1]
+        for j in range(w + 1):
+            for k in range(v + 1):
                 if weight <= j and volume <= k:
-                    dp[j][k] = max(dp[j][k], dp[j - weight]
-                                   [k - volume] + value)
+                    dp[i][j][k] = max(dp[i - 1][j][k], dp[i - 1][j - weight][k - volume] + value)
+                else:
+                    dp[i][j][k] = dp[i - 1][j][k]
 
-    return json.dumps(dp[w][v])
+    return dp[n][w][v]
+
