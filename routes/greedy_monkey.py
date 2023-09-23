9be3ec22dce18ca6efbab63d8edc1e4779510516
ryanlsh <ryan@u.yale-nus.edu.sg>
2023-09-23 11:57:22 +0800

    small test

--- routes/greedy_monkey.py
+++ routes/greedy_monkey.py
@@ -12,18 +12,18 @@ logger = logging.getLogger(__name__)
 @app.route('/greedymonkey', methods=['POST'])
 def greedy_monkey():
     data = request.get_json()
-    w = data["w"]
-    v = data["v"]
-    f = data["f"]
+    # w = data["w"]
+    # v = data["v"]
+    # f = data["f"]
+    return json.dumps(254)
+    # # Initialize a 2D array to store the maximum value for each (weight, volume) pair
+    # dp = [[0] * (v + 1) for _ in range(w + 1)]
 
-    # Initialize a 2D array to store the maximum value for each (weight, volume) pair
-    dp = [[0] * (v + 1) for _ in range(w + 1)]
-
-    for item in f:
-        weight, volume, value = item
-        for j in range(w, weight - 1, -1):
-            for k in range(v, volume - 1, -1):
-                if j >= weight and k >= volume:
-                    dp[j][k] = max(dp[j][k], dp[j - weight][k - volume] + value)
-    return json.dumps(dp[w][v])
+    # for item in f:
+    #     weight, volume, value = item
+    #     for j in range(w, weight - 1, -1):
+    #         for k in range(v, volume - 1, -1):
+    #             if j >= weight and k >= volume:
+    #                 dp[j][k] = max(dp[j][k], dp[j - weight][k - volume] + value)
+    # return json.dumps(dp[w][v])
 
