0dd533d6804223270893c1aa373cba0faae4c7b4
ryanlsh <ryan@u.yale-nus.edu.sg>
2023-09-23 11:48:14 +0800

    fix: optimized solution

--- routes/greedy_monkey.py
+++ routes/greedy_monkey.py
@@ -15,17 +15,15 @@ def greedy_monkey():
     w = data["w"]
     v = data["v"]
     f = data["f"]
-    n = len(f)
-    dp = [[[0] * (v + 1) for _ in range(w + 1)] for _ in range(n + 1)]
 
-    for i in range(1, n + 1):
-        weight, volume, value = f[i - 1]
-        for j in range(w + 1):
-            for k in range(v + 1):
-                if weight <= j and volume <= k:
-                    dp[i][j][k] = max(dp[i - 1][j][k], dp[i - 1][j - weight][k - volume] + value)
-                else:
-                    dp[i][j][k] = dp[i - 1][j][k]
+    # Initialize a 2D array to store the maximum value for each (weight, volume) pair
+    dp = [[0] * (v + 1) for _ in range(w + 1)]
 
-    return json.dumps(dp[n][w][v])
+    for item in f:
+        weight, volume, value = item
+        for j in range(w, weight - 1, -1):
+            for k in range(v, volume - 1, -1):
+                if j >= weight and k >= volume:
+                    dp[j][k] = max(dp[j][k], dp[j - weight][k - volume] + value)
+    return json.dumps(dp[w][v])
 
